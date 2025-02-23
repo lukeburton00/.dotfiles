453c42bd68d305049fb9c3b5e024167b7259ef4d
Luke Burton <lukesburton19@gmail.com>
2025-02-22 23:03:16 -0700

    add powershell

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+# Renders background colors correctly
+$PSStyle.FileInfo.Directory = "`e[96;1m"
+
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
