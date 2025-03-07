4624279ee8b147245c8ff1b00e6448935590274d
Luke Burton <=>
2025-03-06 21:08:42 -0700

    back to base16 for theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,12 +1,11 @@
 return {
     "RRethy/base16-nvim",
-    priority = 1000,
     lazy = false,
+    priority = 1000,
 
-    config = function ()
-        vim.cmd.colorscheme("base16-catppuccin-mocha")
+    config = function()
+        vim.cmd.colorscheme("base16-default-dark")
 
-        -- Hide end of buffer characters
         local hl = vim.api.nvim_get_hl(0, { name = 'Normal' })
         local bg = hl.bg
         if bg then
