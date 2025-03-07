98ba9865aee73b02c09ec2cf860a80bb2c4874e9
Luke Burton <=>
2025-03-06 21:08:28 -0700

    add git info to oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,16 +1,27 @@
 return {
-    'stevearc/oil.nvim',
-    opts = {},
+    {
+        'stevearc/oil.nvim',
+        opts = {},
 
-    lazy = false,
+        lazy = false,
 
-    config = function ()
-        require("oil").setup {
-            view_options = {
-                show_hidden = true
+        config = function ()
+            require("oil").setup {
+                view_options = {
+                    show_hidden = true
+                },
+
+                win_options = {
+                    signcolumn = "yes:2",
+                    statuscolumn = ""
+                }
             }
-        }
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-    end
+        end
+    },
+    {
+        ft = "oil",
+        opts = {},   "FerretDetective/oil-git-signs.nvim",
+    }
 }
