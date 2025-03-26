1331b635df910bfdcf71cb39f62fb11012819565
Luke Burton <=>
2025-03-26 09:44:57 -0600

    switch from harpoon to grapple

---
+++ nvim/lua/plugins/grapple.lua
@@ -0,0 +1,18 @@
+return {
+    "cbochs/grapple.nvim",
+
+    event = 'VeryLazy',
+
+    opts = {
+        scope = 'git',
+    },
+
+    keys = {
+        { "<leader>e", "<cmd>Grapple toggle_tags<cr>", desc = "Open Grapple menu" },
+        { "<leader>a", "<cmd>Grapple tag<cr>", desc = "Add Grapple tag" },
+        { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Go to Grapple tag 1" },
+        { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Go to Grapple tag 2" },
+        { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Go to Grapple tag 3" },
+        { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Go to Grapple tag 4" },
+    }
+}
