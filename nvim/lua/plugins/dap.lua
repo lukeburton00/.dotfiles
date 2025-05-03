a1f289b85216404f368e8424eb588aaee6ce79be
Luke Burton <=>
2025-05-02 21:31:58 -0600

    add debugger

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,30 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
+        { "igorlfs/nvim-dap-view", opts = { winbar = { controls =  { enabled = true}}}}
+    },
+
+    keys = {
+        {
+            "<leader>db",
+            mode = { "n", "v" },
+            "<cmd>DapToggleBreakpoint<cr>"
+        },
+        {
+            "<leader>dv",
+            mode = { "n", "v" },
+            "<cmd>DapViewToggle<cr>"
+        },
+    },
+
+    config = function()
+        require("mason").setup()
+        require("mason-nvim-dap").setup({
+            handlers = {},
+            ensure_installed = {},
+            automatic_installation = false
+        })
+    end
+}
