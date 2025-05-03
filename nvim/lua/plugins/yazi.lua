d83afe0e1705e01477bc68c6427c20dfbfcfdcc1
Luke Burton <=>
2025-05-02 21:32:03 -0600

    add yazi.nvim

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,19 @@
+return {
+  "mikavilpas/yazi.nvim",
+
+  keys = {
+    {
+      "<leader>y",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+  },
+
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
