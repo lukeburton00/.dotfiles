638fa4a6838562b84bed9f2dbd12c3be27ab95bd
Luke Burton <=>
2025-05-12 11:38:04 -0600

    add kulala.nvim

---
+++ nvim/lua/plugins/kulala.lua
@@ -0,0 +1,21 @@
+return {
+    "mistweaverco/kulala.nvim",
+    dependencies = {
+        {
+            "bosvik/http-convert.nvim",
+            cmd = { "ConvertFromOpenApi", "ConvertFromPostman" },
+            opts = {
+                formatter_path = vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "bin", "kulala-fmt"),
+            },
+        },
+    },
+    keys = {
+        { "<leader>Rs", desc = "Send request" },
+        { "<leader>Ra", desc = "Send all requests" },
+        { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = {"http", "rest"},
+    opts = {
+        global_keymaps = true,
+    },
+}
