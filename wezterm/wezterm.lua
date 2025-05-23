61d5bb8179aa48a34aea62d906337e1ccd8557c2
Luke Burton <=>
2025-05-23 11:30:15 -0600

    format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local remaps = require 'remaps'
 
 local config = {}
 config.font = wezterm.font('JetBrains Mono')
-config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.color_scheme = 'Default Dark (base16)'
 config.leader = remaps.leader
@@ -17,7 +17,7 @@ config.max_fps = 240
 config.font_size = 14
 
 if wezterm.target_triple:find("windows") ~= nil then
-    config.default_prog = { 'pwsh.exe'}
+    config.default_prog = { 'pwsh.exe' }
 end
 
 return config
