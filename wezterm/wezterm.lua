61129d8751352352a0e3780cdf7ccf881a577388
Luke Burton <lukesburton19@gmail.com>
2025-06-03 21:33:24 -0600

    apply new custom theme to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,43 @@ config.font = wezterm.font('JetBrains Mono')
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.color_scheme = 'Default Dark (base16)'
-config.leader = remaps.leader
+local custom_nord = {
+    foreground = "#fbffff",
+    background = "#101117",
+    cursor_bg = "#fbffff",
+    cursor_border = "#fbffff",
+    cursor_fg = "#101117",
+    selection_bg = "#465272",
+    selection_fg = "#fbffff",
+    ansi = {
+        "#101117",
+        "#f97081",
+        "#c3eeb8",
+        "#ffe07a",
+        "#84b8f3",
+        "#e698e1",
+        "#acf2ff",
+        "#fbffff",
+    },
+    brights = {
+        "#465272",
+        "#f97081",
+        "#c3eeb8",
+        "#ffe07a",
+        "#84b8f3",
+        "#e698e1",
+        "#72a5f3",
+        "#ffffff",
+    },
+    tab_bar = {
+        background = "#465272",
+        inactive_tab_edge = "#4f5880",
+    },
+}
+
+config.color_schemes = { ["Custom Nord"] = custom_nord }
+config.color_scheme = "Custom Nord"
+
 config.keys = remaps.keys
 
 config.tab_bar_at_bottom = true
