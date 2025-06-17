f1b074fa89949cdbf93e92ce56383858fc506fe7
Luke Burton <=>
2025-06-17 10:45:36 -0600

    just <leader>q for qf list add from snacks picker, qf is too much work

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,6 +3,37 @@ return {
     priority = 1000,
     lazy = false,
 
+    opts = {
+        quickfile = { enabled = true },
+        input = { enabled = true },
+        lazygit = { enabled = true },
+        dashboard = { enabled = true },
+        zen = { enabled = true },
+        picker = {
+            win = {
+                input = {
+                    keys = {
+                        ["<Tab>"] = { "list_down", mode = { "i", "n" } },
+                        ["<S-Tab>"] = { "list_up", mode = { "i", "n" } },
+                        ["<leader>q"] = {
+                            "qflist",
+                            mode = { "n", "i" },
+                            desc = "Send all to quickfix",
+                        },
+                    },
+                },
+            },
+        },
+
+        styles = {
+            zen = {
+                backdrop = {
+                    transparent = false,
+                },
+            },
+        },
+    },
+
     keys = function()
         local Snacks = require("snacks")
         return {
