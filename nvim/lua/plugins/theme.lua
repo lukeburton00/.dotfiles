0f23fbddc03e91fdc0ef5de80a2cc95642a8085f
Luke Burton <=>
2025-06-24 10:11:13 -0600

    remove commented out themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,17 +1,3 @@
--- return {
---     "dgox16/oldworld.nvim",
---     config = function()
---         vim.cmd.colorscheme("oldworld")
---     end,
--- }
-
--- return {
---     "projekt0n/github-nvim-theme",
---     config = function()
---         vim.cmd.colorscheme("github_dark_default")
---     end
--- }
-
 return {
     "vague2k/vague.nvim",
     config = function()
