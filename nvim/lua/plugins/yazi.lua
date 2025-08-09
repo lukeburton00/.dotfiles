2ca96ea74124ab035df7dd7f8d870b7c5970ab79
Luke Burton <=>
2025-08-08 22:15:05 -0600

    yeah yazi is better than fyler for now

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,10 @@
+return {
+    "mikavilpas/yazi.nvim",
+    lazy = false,
+    opts = {
+        open_for_directories = true,
+    },
+    keys = {
+        { "-", "<cmd>Yazi toggle<cr>", desc = "Yazi" },
+    },
+}
