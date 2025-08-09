caf9cf2e72bc8921d341bde69a21d0a329324514
Luke Burton <=>
2025-08-08 22:14:31 -0600

    auto reload changed files in nvim

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -1,2 +1,9 @@
 -- Expand 'cc' into 'CodeCompanion' in the command line
 vim.cmd([[cab cc CodeCompanion]])
+
+-- auto reload buffers on file changes
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = { "*" },
+})
