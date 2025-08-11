2d94ac08b43694b22c7bdbc9056421d2c7d5a072
Luke Burton <=>
2025-08-11 13:25:22 -0600

    no need for global marks when grapple is at play

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -1,24 +1,9 @@
 vim.g.mapleader = " "
 
--- all marks are global marks
-local prefixes = "m'"
-local letters = "abcdefghijklmnopqrstuvwxyz"
-for i = 1, #prefixes do
-    local prefix = prefixes:sub(i, i)
-    for j = 1, #letters do
-        local lower_letter = letters:sub(j, j)
-        local upper_letter = string.upper(lower_letter)
-        vim.keymap.set({ "n", "v" }, prefix .. lower_letter, prefix .. upper_letter, { desc = "Mark " .. upper_letter })
-    end
-end
+vim.keymap.set("n", "Q", "<nop>")
 
--- keep the cursor centered when making big jumps
+-- center cursor after search and scroll
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("n", "Q", "<nop>")
-
-vim.keymap.set("n", "<leader>qo", vim.cmd.copen)
-vim.keymap.set("n", "<leader>qc", vim.cmd.cclose)
