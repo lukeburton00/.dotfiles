c9d0fbfe11bdc568451f1fc79e66c9fa83dd929f
Luke Burton <=>
2025-08-11 13:22:39 -0600

    modernize opts. drop the lazy loading of the clipboard

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -1,74 +1,21 @@
-vim.opt.nu = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 50
-
+vim.o.nu = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.updatetime = 50
+vim.o.showmode = false
+vim.o.showtabline = 0
+vim.o.signcolumn = "yes"
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
 vim.diagnostic.config({ virtual_text = true })
-
-vim.opt.showmode = false
-
-vim.opt.showtabline = 0
-vim.opt.winborder = "rounded"
-
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-    once = true,
-    callback = function()
-        if vim.fn.has("win32") == 1 or vim.fn.has("wsl") == 1 then
-            vim.g.clipboard = {
-                copy = {
-                    ["+"] = "win32yank.exe -i --crlf",
-                    ["*"] = "win32yank.exe -i --crlf",
-                },
-                paste = {
-                    ["+"] = "win32yank.exe -o --lf",
-                    ["*"] = "win32yank.exe -o --lf",
-                },
-            }
-        elseif vim.fn.has("unix") == 1 then
-            if vim.fn.executable("xclip") == 1 then
-                vim.g.clipboard = {
-                    copy = {
-                        ["+"] = "xclip -selection clipboard",
-                        ["*"] = "xclip -selection clipboard",
-                    },
-                    paste = {
-                        ["+"] = "xclip -selection clipboard -o",
-                        ["*"] = "xclip -selection clipboard -o",
-                    },
-                }
-            elseif vim.fn.executable("xsel") == 1 then
-                vim.g.clipboard = {
-                    copy = {
-                        ["+"] = "xsel --clipboard --input",
-                        ["*"] = "xsel --clipboard --input",
-                    },
-                    paste = {
-                        ["+"] = "xsel --clipboard --output",
-                        ["*"] = "xsel --clipboard --output",
-                    },
-                }
-            end
-        end
-
-        vim.opt.clipboard = "unnamedplus"
-    end,
-    desc = "Lazy load clipboard",
-})
