4b6706f76874d4a63aada21dd3aa8973a7e93ac3
Luke Burton <=>
2025-08-14 11:45:42 -0600

    refactor wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,56 +1,21 @@
 local wezterm = require "wezterm"
 
-local config = {}
-config.force_reverse_video_cursor = true
-config.font = wezterm.font("JetBrains Mono")
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+local config = {
+    color_schemes = { ["custom"] = require("theme") },
+    color_scheme = "custom",
+    font = wezterm.font("JetBrains Mono"),
 
-local vague = {
-    foreground = "#cdcdcd",
-    background = "#141415",
+    force_reverse_video_cursor = true,
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 
-    cursor_bg = "#cdcdcd",
-    cursor_fg = "#141415",
-    cursor_border = "#cdcdcd",
+    tab_bar_at_bottom = true,
+    hide_tab_bar_if_only_one_tab = true,
 
-    selection_fg = "#cdcdcd",
-    selection_bg = "#252530",
-
-    scrollbar_thumb = "#252530",
-    split = "#252530",
-
-    ansi = {
-        "#252530",
-        "#d8647e",
-        "#7fa563",
-        "#f3be7c",
-        "#6e94b2",
-        "#bb9dbd",
-        "#aeaed1",
-        "#cdcdcd",
-    },
-    brights = {
-        "#606079",
-        "#e08398",
-        "#99b782",
-        "#f5cb96",
-        "#8ba9c1",
-        "#c9b1ca",
-        "#bebeda",
-        "#d7d7d7",
-    },
+    max_fps = 240,
+    font_size = 18,
 }
 
-config.color_schemes = { ["vague"] = vague }
-config.color_scheme = "vague"
-
-config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
-
-config.max_fps = 240
-config.font_size = 18
-
-if wezterm.target_triple:find("windows") ~= nil then
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { "pwsh.exe" }
 end
 
