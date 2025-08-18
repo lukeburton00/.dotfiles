8418b2f9b38f1c1bae327f0b7e21ea948035ff1e
Luke Burton <=>
2025-08-18 14:48:07 -0600

    toml theme for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,6 @@ local wezterm = require "wezterm"
 local config = {
     font = wezterm.font("JetBrains Mono"),
     color_scheme = "vague",
-    force_reverse_video_cursor = true,
     harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 
     tab_bar_at_bottom = true,
@@ -12,47 +11,7 @@ local config = {
     max_fps = 240,
     font_size = 18,
 
-    window_frame = {
-        active_titlebar_bg = "#141415",
-        inactive_titlebar_bg = "#141415",
-    },
-
-    colors = {
-        tab_bar = {
-            background = "#141415",
-            active_tab = {
-                bg_color = "#252530",
-                fg_color = "#cdcdcd",
-                intensity = "Normal",
-                underline = "None",
-                italic = false,
-                strikethrough = false,
-            },
-            inactive_tab = {
-                bg_color = "#141415",
-                fg_color = "#606079",
-                intensity = "Normal",
-                underline = "None",
-                italic = false,
-                strikethrough = false,
-            },
-            inactive_tab_hover = {
-                bg_color = "#252530",
-                fg_color = "#cdcdcd",
-                italic = false,
-            },
-            new_tab = {
-                bg_color = "#141415",
-                fg_color = "#6e94b2",
-            },
-            new_tab_hover = {
-                bg_color = "#252530",
-                fg_color = "#8ba9c1",
-                italic = false,
-            },
-            inactive_tab_edge = "#252530",
-        },
-    },
+    use_fancy_tab_bar = false,
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
