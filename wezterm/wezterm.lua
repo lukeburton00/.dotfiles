e2adb0848423ca7869b32d9123a11c4e5258273c
Luke Burton <=>
2025-08-21 11:04:47 -0600

    switch to official wezterm vague theme :D (with adjusted bg color)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,16 +1,18 @@
 local wezterm = require "wezterm"
+local theme = require("lua/vague")
 
 local config = {
     color_scheme = "vague",
     harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+
+    colors = theme.colors(),
+    window_frame = theme.window_frame(),
 
     tab_bar_at_bottom = true,
     hide_tab_bar_if_only_one_tab = true,
 
     max_fps = 240,
     font_size = 18,
-
-    use_fancy_tab_bar = false,
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
