b156cfb8be39c4087f2207d79c10456788d9fa3c
Luke Burton <=>
2025-08-25 11:05:34 -0600

    no auto comments on newline

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -7,3 +7,10 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
     command = "if mode() != 'c' | checktime | endif",
     pattern = { "*" },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    end,
+})
