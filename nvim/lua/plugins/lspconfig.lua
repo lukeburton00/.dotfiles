aca182feb8c827f76f19ffaceed7b7d562aa7160
Luke Burton <=>
2025-09-15 09:57:41 -0600

    update plugins, drop lazydev

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,41 @@ return {
         vim.lsp.config("*", {
             capabilities = require("blink.cmp").get_lsp_capabilities(),
         })
+        vim.lsp.config("lua_ls", {
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if
+                        path ~= vim.fn.stdpath("config")
+                        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+                    then
+                        return
+                    end
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        version = "LuaJIT",
+                        path = {
+                            "lua/?.lua",
+                            "lua/?/init.lua",
+                        },
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME .. "/lua",
+                            vim.env.VIMRUNTIME .. "/lua/vim/lsp",
+                            "${3rd}/luv/library",
+                            "${3rd}/busted/library",
+                        },
+                    },
+                })
+            end,
+            settings = {
+                Lua = {},
+            },
+        })
 
         vim.lsp.enable({
             "gopls",
