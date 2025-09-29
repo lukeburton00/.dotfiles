4f605cb9ed49d8d3330a55a59e2f613885abc457
Luke Burton <lukesburton19@gmail.com>
2025-09-28 23:17:06 -0600

    smaller font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,7 +12,7 @@ local config = {
     hide_tab_bar_if_only_one_tab = true,
 
     max_fps = 240,
-    font_size = 18,
+    font_size = 16,
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
