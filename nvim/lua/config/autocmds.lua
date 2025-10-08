d7801a45b63b1a3531ceb4ce15d50568d827a8fd
Luke Burton <=>
2025-10-08 10:18:38 -0600

    auto reload files using periodic check

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -106,3 +106,24 @@ vim.api.nvim_create_autocmd("TermOpen", {
         vim.cmd([[startinsert]])
     end,
 })
+
+-- Periodically check for changes every 2 seconds
+local autoread_timer = vim.loop.new_timer()
+
+autoread_timer:start(
+    2000,
+    2000,
+    vim.schedule_wrap(function()
+        -- Only check when Neovim has focus and you're not in command mode
+        if vim.fn.getcmdwintype() == "" and vim.fn.mode() ~= "c" then
+            pcall(vim.cmd, "checktime")
+        end
+    end)
+)
+
+-- Notify only when reload actually happened
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    callback = function()
+        vim.notify("File reloaded from disk", vim.log.levels.INFO, { title = "AutoRead" })
+    end,
+})
