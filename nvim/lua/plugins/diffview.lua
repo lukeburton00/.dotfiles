3fb3b772aeb8d8a7cb4449f4eba34a6958bdd134
Luke Burton <=>
2025-10-17 11:42:53 -0600

    simplify diff review

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -3,7 +3,10 @@ return {
     dependencies = {
         "nvim-mini/mini.nvim",
     },
-    cmd = "DiffviewOpen",
+    cmd = {
+        "DiffviewOpen",
+        "DiffReview",
+    },
     keys = {
         {
             "<leader>dv",
@@ -21,11 +24,9 @@ return {
     },
     opts = {},
     config = function()
+        -- compare current branch to main
         vim.api.nvim_create_user_command("DiffReview", function()
-            local main_branch = vim.fn
-                .system("git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'")
-                :gsub("\n", "")
-            vim.cmd("DiffviewOpen " .. main_branch .. "...HEAD")
+            vim.cmd("DiffviewOpen origin/HEAD...HEAD --imply-local")
         end, {})
     end,
 }
