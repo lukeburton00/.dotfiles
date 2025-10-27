e3aec84a1d1517eedeff770dec5b38fb9652d64f
Luke Burton <=>
2025-10-27 17:08:19 -0600

    back to blink

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,72 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
+    event = "InsertEnter",
+    version = "1.*",
+    opts = {
+        keymap = {
+            preset = "none",
+            ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+            ["<CR>"] = { "select_and_accept", "fallback" },
+            ["<Esc>"] = {
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc><Esc>", true, false, true), "i", false),
+            },
+        },
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = "mono",
+        },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+        completion = {
+            list = {
+                selection = {
+                    preselect = false,
+                    auto_insert = false,
+                },
+            },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 250,
+                window = { border = "rounded" },
+            },
+            ghost_text = { enabled = true },
+            menu = {
+                border = "rounded",
+                draw = {
+                    components = {
+                        kind_icon = {
+                            text = function(ctx)
+                                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return kind_icon
+                            end,
+                            -- (optional) use highlights from mini.icons
+
+                            highlight = function(ctx)
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return hl
+                            end,
+                        },
+                        kind = {
+                            -- (optional) use highlights from mini.icons
+                            highlight = function(ctx)
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return hl
+                            end,
+                        },
+                    },
+                },
+            },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+        signature = {
+            enabled = true,
+            window = { border = "rounded", show_documentation = true },
+        },
+    },
+    opts_extend = { "sources.default" },
+}
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,8 +1,5 @@
 return {
     "nvim-mini/mini.nvim",
-    dependencies = {
-        "rafamadriz/friendly-snippets",
-    },
     version = "*",
     event = "BufReadPre",
     config = function()
@@ -15,30 +12,5 @@ return {
         require("mini.icons").setup()
         require("mini.icons").mock_nvim_web_devicons()
         require("mini.icons").tweak_lsp_kind()
-        require("mini.snippets").setup()
-        require("mini.completion").setup()
-        vim.keymap.set("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true })
-        vim.keymap.set("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
-        vim.keymap.set("i", "<CR>", function()
-            if vim.fn.pumvisible() == 0 then
-                return require("mini.pairs").cr()
-            elseif vim.fn.complete_info()["selected"] ~= -1 then
-                return vim.api.nvim_replace_termcodes("<C-y>", true, true, true)
-            else
-                return vim.api.nvim_replace_termcodes("<C-y><CR>", true, true, true)
-            end
-        end, { expr = true })
-        vim.api.nvim_create_autocmd("BufEnter", {
-            callback = function(args)
-                local ft = vim.bo[args.buf].filetype
-                local bt = vim.bo[args.buf].buftype
-
-                if bt == "prompt" or ft == "snacks_picker" then
-                    vim.b.minicompletion_disable = true
-                else
-                    vim.b.minicompletion_disable = false
-                end
-            end,
-        })
     end,
 }
