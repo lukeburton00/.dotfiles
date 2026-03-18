6d98e43755bde3eb08680833f00c91171ee4d7fd
Luke Burton <lukesburton19@gmail.com>
2026-03-17 22:16:11 -0600

    nvim: catppuccin theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,6 +1,14 @@
 return {
-    "vague2k/vague.nvim",
-    config = function()
-        vim.cmd.colorscheme("vague")
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+        background = {
+            dark = "mocha",
+        },
+    },
+
+    init = function()
+        vim.cmd.colorscheme("catppuccin-nvim")
     end,
 }
