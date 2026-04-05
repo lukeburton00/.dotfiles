f6ce81333ebe15379000cb07637ceaf8bd140cec
Luke Burton <lukesburton19@gmail.com>
2026-04-05 11:08:33 -0600

    nvim: add lsp keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -37,3 +37,12 @@ vim.lsp.config("lua_ls", {
                 },
             },
         })
+    end,
+    settings = {
+        Lua = {},
+    },
+})
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gk", vim.diagnostic.open_float)
