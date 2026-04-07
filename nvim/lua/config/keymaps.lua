7609f7b70af873cd4fc645f2a8bec6016043aed1
Luke Burton <lukesburton19@gmail.com>
2026-04-07 15:42:28 -0600

    nvim: keymaps for lazygit and bare terminal

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,52 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- lazygit
+vim.keymap.set("n", "<leader>g", function()
+    vim.cmd("vsplit | wincmd l | term lazygit")
+
+    local buf = vim.api.nvim_get_current_buf()
+
+    vim.api.nvim_create_autocmd("TermClose", {
+        buffer = buf,
+        callback = function()
+            vim.schedule(function()
+                vim.cmd("bd!")
+            end)
+        end,
+    })
+
+    vim.cmd("startinsert")
+end)
+
+vim.keymap.set("n", "<leader>t", function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == "terminal" then
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                if vim.api.nvim_win_get_buf(win) == buf then
+                    vim.api.nvim_set_current_win(win)
+                    vim.cmd.startinsert()
+                    return
+                end
+            end
+        end
+    end
+
+    vim.cmd("belowright 15split")
+    vim.cmd("term")
+    local buf = vim.api.nvim_get_current_buf()
+
+    vim.api.nvim_create_autocmd("TermClose", {
+        buffer = buf,
+        callback = function()
+            vim.schedule(function()
+                if vim.api.nvim_buf_is_loaded(buf) then
+                    vim.cmd("bd!")
+                end
+            end)
+        end,
+    })
+
+    vim.cmd.startinsert()
+end)
