e38340911b00208baf186d64246c7559c31582b6
Luke Burton <=>
2026-06-26 11:04:17 -0600

    wezterm: remove colorscheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,39 +16,5 @@ elseif wezterm.target_triple:find("linux") ~= nil then
 elseif wezterm.target_triple:find("darwin") ~= nil then
     config.font_size = 18
 end
-config.color_schemes = config.color_schemes or {}
-config.color_schemes["dustyrose"] = {
-    foreground = "#c8c0d0",
-    background = "#1a1b2a",
-    cursor_fg = "#c8c0d0",
-    cursor_bg = "#e8a0b0",
-    cursor_border = "#e8a0b0",
-    selection_fg = "#c8c0d0",
-    selection_bg = "#c8c0d0",
-
-    ansi = {
-        "#6e606a",
-        "#d08890",
-        "#8cc080",
-        "#c8c0d0",
-        "#e8a0b0",
-        "#b86878",
-        "#b8a0d0",
-        "#c8c0d0",
-    },
-
-    brights = {
-        "#6e606a",
-        "#d08890",
-        "#8cc080",
-        "#c8c0d0",
-        "#e8a0b0",
-        "#b86878",
-        "#b8a0d0",
-        "#c8c0d0",
-    },
-}
-
-config.color_scheme = "dustyrose"
 
 return config
