fe9bf8dc0084c2bb45b7fc3f9d0d57c0d6b71a6a
Luke Burton <lukesburton19@gmail.com>
2026-09-15 10:23:16 -0600

    nvim: add keymap to open lazygit inside nvim

--- nvim/.config/nvim/lua/config/keys.lua
+++ nvim/.config/nvim/lua/config/keys.lua
@@ -6,3 +6,23 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- LazyGit
+vim.keymap.set("n", "<leader>g", function()
+    local group = vim.api.nvim_create_augroup("LazyGitTermClose", {
+        clear = true,
+    })
+
+    vim.api.nvim_create_autocmd("TermClose", {
+        group = group,
+        once = true,
+        callback = function()
+            vim.schedule(function()
+                vim.api.nvim_feedkeys(vim.keycode("<CR>"), "n", false)
+            end)
+        end,
+    })
+
+    vim.cmd.term("lazygit")
+    vim.cmd.startinsert()
+end)
