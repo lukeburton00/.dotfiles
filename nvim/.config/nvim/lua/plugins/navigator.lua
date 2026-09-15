eb3a8f7efaa8a3224218af2a888e115b3bb398f5
Luke Burton <lukesburton19@gmail.com>
2026-09-15 10:28:00 -0600

    nvim: add terminal mode to navigator keymaps

--- nvim/.config/nvim/lua/plugins/navigator.lua
+++ nvim/.config/nvim/lua/plugins/navigator.lua
@@ -5,18 +5,22 @@ return {
         {
             "<C-h>",
             "<cmd>NavigatorLeft<CR>",
+            mode = { "n", "t" },
         },
         {
             "<C-j>",
             "<cmd>NavigatorDown<CR>",
+            mode = { "n", "t" },
         },
         {
             "<C-k>",
             "<cmd>NavigatorUp<CR>",
+            mode = { "n", "t" },
         },
         {
             "<C-l>",
             "<cmd>NavigatorRight<CR>",
+            mode = { "n", "t" },
         },
     },
 }
